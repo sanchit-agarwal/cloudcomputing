a6b96467fbd62083c24d3428ede671959e65ad67
Sanchit Agarwal <sanchit.agarwal1331@outlook.com>
2022-03-28 09:11:07 -0700

    Initial Commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+
+def init(test_config=None):
+	app = Flask(__name__, instance_relative_config=True)
+	
+	if test_config is None:
+		app.config.from_pyfile('config.py', silent=True)
+	else:
+		app.config.from_mapping(test_config)
+	
+	try:
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
+	
+	@app.route('/hello')
+	def hello():
+		return 'Hello, World!'
+	
+	return app
